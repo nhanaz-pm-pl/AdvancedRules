eb001bc74fc663bca2647c17c709244054df0f5f
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-05-31 11:49:47 +0700

    minor change

--- src/NhanAZ/AdvancedRules/Main.php
+++ src/NhanAZ/AdvancedRules/Main.php
@@ -7,7 +7,6 @@ namespace NhanAZ\AdvancedRules;
 use pocketmine\utils\Config;
 use pocketmine\player\Player;
 use pocketmine\event\Listener;
-use pocketmine\utils\TextFormat;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\player\PlayerJoinEvent;
 use dktapps\pmforms\CustomForm;
@@ -17,50 +16,47 @@ use dktapps\pmforms\CustomFormResponse;
 
 class Main extends PluginBase implements Listener {
 
-	protected Config $cfg;
-
 	protected Config $data;
 
 	protected function onEnable(): void {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->saveDefaultConfig();
 		$this->data = new Config($this->getDataFolder() . "Data.json", Config::JSON);
-		$this->cfg = $this->getConfig();
 	}
 
-	private function CheckData(Player $player, bool $kickMode = true): void {
+	private function checkData(Player $player, bool $kickMode = true): void {
 		if (!$this->data->exists($player->getName())) {
 			if ($this->getConfig()->get("KickMode", true)) {
 				if ($kickMode) {
-					$player->kick(TextFormat::colorize($this->getConfig()->get("KickMessage", "&e» &cPlease agree to the server's rules!")));
+					$player->kick($this->getConfig()->get("KickMessage"));
 				}
 			}
-			$player->sendForm($this->AdvancedRulesForm());
+			$player->sendForm($this->advancedRulesForm());
 		}
 	}
 
 	public function onJoin(PlayerJoinEvent $event): void {
 		$player = $event->getPlayer();
-		$this->CheckData($player, false);
+		$this->checkData($player, false);
 	}
 
-	private function AdvancedRulesForm(): CustomForm {
+	private function advancedRulesForm(): CustomForm {
 		return new CustomForm(
-			TextFormat::colorize($this->cfg->get("TitleForm", "AdvancedRules")),
+			$this->getConfig()->get("TitleForm", "AdvancedRules"),
 			[
-				new Label("content", TextFormat::colorize($this->cfg->get("Rules", "&8» &rAdvancedRules by NhanAZ.\n&8» &rEdit the &6config.yml &rFile and restart server."))),
-				new Toggle("switch", TextFormat::colorize($this->cfg->get("AgreeButon", "I agree")), false)
+				new Label("content", $this->getConfig()->get("Rules")),
+				new Toggle("switch", $this->getConfig()->get("AgreeButon"), false)
 			],
 			function (Player $submitter, CustomFormResponse $response): void {
 				if ($response->getBool("switch")) {
 					$this->data->set($submitter->getName(), true);
 					$this->data->save();
-					$submitter->sendMessage(TextFormat::colorize($this->cfg->get("AgreedMessage", "[&2ServerName&f]&8»&aYou accepted the rules, have fun!")));
+					$submitter->sendMessage($this->getConfig()->get("AgreedMessage"));
 				}
-				$this->CheckData($submitter, true);
+				$this->checkData($submitter, true);
 			},
 			function (Player $submitter): void {
-				$this->CheckData($submitter, false);
+				$this->checkData($submitter, false);
 			}
 		);
 	}
