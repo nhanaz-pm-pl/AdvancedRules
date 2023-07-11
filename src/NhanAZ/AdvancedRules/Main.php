9f1b3b7a46a029746434633c8ecdd6e6ea9a28d6
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-07-11 21:32:41 +0700

    Added /advancedrules - Show rules form

--- src/NhanAZ/AdvancedRules/Main.php
+++ src/NhanAZ/AdvancedRules/Main.php
@@ -12,6 +12,9 @@ use dktapps\pmforms\CustomForm;
 use dktapps\pmforms\element\Label;
 use dktapps\pmforms\element\Toggle;
 use dktapps\pmforms\CustomFormResponse;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
 use Symfony\Component\Filesystem\Path;
 
 class Main extends PluginBase implements Listener {
@@ -40,23 +43,46 @@ class Main extends PluginBase implements Listener {
 		$this->checkData($event->getPlayer(), false);
 	}
 
-	private function advancedRulesForm(): CustomForm {
-		return new CustomForm(
-			$this->getConfig()->get("TitleForm", "AdvancedRules"),
-			[
-				new Label("content", $this->getConfig()->get("Rules")),
-				new Toggle("switch", $this->getConfig()->get("AgreeButon"), false)
-			],
-			function (Player $submitter, CustomFormResponse $response): void {
-				if ($response->getBool("switch")) {
-					file_put_contents($this->getDataFolder() . "data/" . $submitter->getName(), "");
-					$submitter->sendMessage($this->getConfig()->get("AgreedMessage"));
+	private function advancedRulesForm(bool $agreeButon = true): CustomForm {
+		if ($agreeButon) {
+			return new CustomForm(
+				$this->getConfig()->get("TitleForm", "AdvancedRules"),
+				[
+					new Label("content", $this->getConfig()->get("Rules")),
+					new Toggle("switch", $this->getConfig()->get("AgreeButon"), false)
+				],
+				function (Player $submitter, CustomFormResponse $response): void {
+					if ($response->getBool("switch")) {
+						file_put_contents($this->getDataFolder() . "data/" . $submitter->getName(), "");
+						$submitter->sendMessage($this->getConfig()->get("AgreedMessage"));
+					}
+					$this->checkData($submitter, true);
+				},
+				function (Player $submitter): void {
+					$this->checkData($submitter, false);
 				}
-				$this->checkData($submitter, true);
-			},
-			function (Player $submitter): void {
-				$this->checkData($submitter, false);
+			);
+		} else {
+			return new CustomForm(
+				$this->getConfig()->get("TitleForm", "AdvancedRules"),
+				[
+					new Label("content", $this->getConfig()->get("Rules")),
+				],
+				function (Player $submitter, CustomFormResponse $response): void {
+				}
+			);
+		}
+	}
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+		if ($command->getName() === "advancedrules") {
+			if (!$sender instanceof Player) {
+				$sender->sendMessage(TextFormat::RED . "You can't use this command in the terminal!");
+				return true;
 			}
-		);
+			$sender->sendForm(self::advancedRulesForm(agreeButon: false));
+			return true;
+		}
+		return false;
 	}
 }
